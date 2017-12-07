56c634343e9df9b75202c79056dd2bca07d7d7c8
Lukas Jamann <lukasjamann@me.com>
2017-12-07 15:43:36 +0100

    Änderung für Blob

--- Dialogs/SuperDialog.cs
+++ Dialogs/SuperDialog.cs
@@ -11,6 +11,11 @@ using Microsoft.Bot.Builder.ConnectorEx;
 using System.Data.SqlClient;
 using System.Data;
 using System.Threading;
+using Microsoft.WindowsAzure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Blob; // Namespace for Blob storage types
+using System.Configuration;
+using System.Net.Http.Headers;
 
 namespace Bewerbungs.Bot.Luis
 {
@@ -61,6 +66,39 @@ namespace Bewerbungs.Bot.Luis
 
         [LuisIntent("Farewell")]
         [LuisIntent("")]
+        public async Task Upload(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
+        {
+            var message = await activity;
+            string FileName = message.Attachments[0].Name;
+            var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["AzureWebJobsStorage"]);
+            if (message.Attachments != null && message.Attachments.Any())
+            {
+                var attachment = message.Attachments.First();
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    // Skype & MS Teams attachment URLs are secured by a JwtToken, so we need to pass the token from our bot.
+                    if ((message.ChannelId.Equals("skype", StringComparison.InvariantCultureIgnoreCase) || message.ChannelId.Equals("msteams", StringComparison.InvariantCultureIgnoreCase))
+                        && new Uri(attachment.ContentUrl).Host.EndsWith("skype.com"))
+                    {
+                        var token = await new MicrosoftAppCredentials().GetTokenAsync();
+                        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    }
+
+                    var responseMessage = await httpClient.GetAsync(attachment.ContentUrl);
+
+                    var contentLenghtBytes = responseMessage.Content.Headers.ContentLength;
+
+                    await context.PostAsync($"Attachment of {attachment.ContentType} type and size of {contentLenghtBytes} bytes received.");
+                }
+            }
+            else
+            {
+                await context.PostAsync("Hi there! I'm a bot created to show you how I can receive message attachments, but no attachment was sent to me. Please, try again sending a new message including an attachment.");
+            }
+            await context.PostAsync(message);
+
+            context.Wait(this.MessageReceived);
+        }
         [LuisIntent("None")]
         public async Task None(IDialogContext context, LuisResult result)
         {
