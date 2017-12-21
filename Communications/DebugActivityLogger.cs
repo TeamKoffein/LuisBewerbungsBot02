8a8d254c97624b996c6ba1439a308e6f2b9163f8
Lukas Jamann <lukasjamann@me.com>
2017-12-21 17:40:29 +0100

    Logger in der BlobStorage

--- Communications/DebugActivityLogger.cs
+++ Communications/DebugActivityLogger.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 using System.Web;
 using System.IO;
 using System.Text;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
+using System.Configuration;
 
 namespace Bewerbungs.Bot.Luis
 {
@@ -17,9 +20,33 @@ namespace Bewerbungs.Bot.Luis
         public async Task LogAsync(IActivity activity)
         {
             Debug.WriteLine($"From:{activity.From.Id} - To:{activity.Recipient.Id} - Message:{activity.AsMessageActivity()?.Text}");
-            string path = "Logging/" + activity.Conversation.Id + ".txt";
+            string path = activity.Conversation.Id + ".txt";
             string destPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path);
+            CloudStorageAccount csa = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            CloudBlobClient blobClient = csa.CreateCloudBlobClient();
+            CloudBlobContainer container=blobClient.GetContainerReference("logging");
+            CloudBlockBlob blob = container.GetBlockBlobReference(path);
+            string contents = $"From:{activity.From.Id} - To:{activity.Recipient.Id} - Message:{activity.AsMessageActivity()?.Text}";
 
+            string oldContent;
+            if (!blob.Exists())
+            {
+                oldContent = "";
+            }
+            else
+            {
+                using (StreamReader reader = new StreamReader(blob.OpenRead()))
+                {
+                    oldContent = reader.ReadToEnd();
+                }
+            }
+
+            using (StreamWriter writer = new StreamWriter(blob.OpenWrite()))
+            {
+                writer.Write(oldContent + Environment.NewLine);
+                writer.Write(contents + Environment.NewLine);
+            }
+        /*
             if (!File.Exists(path))
             {
                 // Create a file to write to.
@@ -43,7 +70,7 @@ namespace Bewerbungs.Bot.Luis
             finally
             {
                 Console.WriteLine("Executing finally block.");
-            }
+            }*/
         }
     }
 }
