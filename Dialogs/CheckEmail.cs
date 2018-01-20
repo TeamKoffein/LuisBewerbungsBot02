163eb5a6736f2393aa1bcc7eb7fb11a524c4b7dc
Lukas Jamann <lukasjamann@me.com>
2018-01-20 19:01:12 +0100

    Warnings weggemacht

--- Dialogs/CheckEmail.cs
+++ Dialogs/CheckEmail.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.Bot.Builder.Dialogs;
-using System;
+//using System;
 using System.Threading.Tasks;
 using Microsoft.Bot.Connector;
 using Bewerbungs.Bot.Luis;
